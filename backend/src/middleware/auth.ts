0b46a89c3bb5196a5e8b83f7474edb7d65befb50
Jirayu <jirayu.saeng@gmail.com>
2024-06-12 12:27:03 -0700

    finished authentication

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,1 +1,45 @@
 import { verifyToken } from "../lib/helper"
+
+// "student", "instructor", "admin"
+export const authMiddleWare = (roles: string[]) => {
+  return function (req: any, res: any, next: any) {
+    const headers = req.headers
+    if (!headers["authorization"]) {
+      return res.status(401).send({
+        error: "No token provided",
+      })
+    }
+
+    const token = headers["authorization"].split(" ")[1]
+    const decoded: any = verifyToken(token)
+    console.log(decoded)
+    if (!decoded) {
+      return res.status(401).send({
+        error: "Unauthorized",
+      })
+    }
+    const user: any = decoded.user
+    req.user = user
+    // role authorization
+    if (roles.length && !roles.includes(user.role)) {
+      return res.status(403).send({
+        error: "Forbidden",
+      })
+    }
+    next()
+  }
+}
+
+export const getExactRole = (req: any, res: any, next: any) => {
+  const userId = req.params.userId
+  const user = req.user
+  if (user.role === "admin") {
+    return next()
+  }
+  if (userId !== req.user.id) {
+    return res.status(403).send({
+      error: "Forbidden",
+    })
+  }
+  next()
+}
--- backend/src/user/user.route.ts
+++ backend/src/user/user.route.ts
@@ -1,13 +1,19 @@
+import { authMiddleWare, getExactRole } from "../middleware/auth"
+
 import type { Router } from "express"
 import express from "express"
 import { userController } from "./user.controller"
 
 const router: Router = express.Router()
 
-router.get("/", userController.getMe)
+router.get("/", authMiddleWare(["student", "instructor", "admin"]), userController.getMe)
 router.post("/", userController.createUser)
 router.post("/login", userController.login)
-router.get("/:userId", userController.getUser)
-
+router.get(
+  "/:userId",
+  authMiddleWare(["student", "instructor", "admin"]),
+  getExactRole,
+  userController.getUser
+)
 
 export default router
