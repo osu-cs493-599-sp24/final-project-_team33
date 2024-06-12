3fedff9e3b13375b5aaaa8293a4342ef7a2d4b3d
Jirayu <jirayu.saeng@gmail.com>
2024-06-12 13:02:11 -0700

    update

--- backend/src/route.main.ts
+++ backend/src/route.main.ts
@@ -1,17 +1,15 @@
 import type { Router } from "express"
-// import businessRouter from "./business/business.route"
-import express from "express"
-import userRouter from "./user/user.route"
 import assignmentRouter from "./assignment/assignment.route"
-import submissionRouter from "./submission/submission.route"
-
 import courseRouter from "./course/course.route"
+import express from "express"
+import submissionRouter from "./submission/submission.route"
+import userRouter from "./user/user.route"
+
 const router: Router = express.Router()
 
 router.use("/users", userRouter)
 router.use("/assignments", assignmentRouter)
 router.use("/submissions", submissionRouter)
-
 router.use("/courses", courseRouter)
 
 export default router
